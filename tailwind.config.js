e61e572b8d031629890c573cc5ec0853680f3a56
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-15 23:37:12 +0300

    lint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
-const colors = require("tailwindcss/colors");
+/* eslint-env node */
+const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+
   theme: {
     extend: {
       colors: {
         primary: colors.yellow[500],
         secondary: colors.cyan[500],
-        "text-color": colors.slate[200],
-      },
-    },
+        'text-color': colors.slate[200]
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
