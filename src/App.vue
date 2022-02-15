c543328d5258959d6bfc5fd5a4b3665e1c75af57
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-15 23:40:38 +0300

    feature: add Progress and SelectHotel to homepage

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
-  <TheHeader />
-  <main class="mx-auto">
+  <TheNavBar />
+  <main
+    class="relative mx-auto mt-20 flex w-full flex-col items-center justify-center gap-10 lg:w-3/4"
+  >
     <TheProgress />
+    <div class="relative w-full rounded-md bg-slate-700/30">
+      <Suspense>
+        <template #default>
+          <SelectHotel />
+        </template>
+        <template #fallback>
+          <div class="mx-5 my-8">
+            <TheSelectbox placeholder="Loading" disabled />
+          </div>
+        </template>
+      </Suspense>
+    </div>
   </main>
 </template>
 
 <script setup lang="ts">
-import TheHeader from './components/TheHeader.vue'
+import { useStore } from 'vuex'
+import TheNavBar from './components/TheNavBar.vue'
 import TheProgress from './components/TheProgress.vue'
+import type State from './store/state.model'
+import SelectHotel from './components/SelectHotel.vue'
+import TheSelectbox from './components/TheSelectbox.vue'
+
+const store = useStore<State>()
 </script>
 
 <style>
