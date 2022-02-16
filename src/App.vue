8b9626eef81573a77e3e0463ed952ed6b1806975
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-16 19:05:34 +0300

    feature: add Toastr and hadle error

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <TheNavBar />
-  <main
-    class="relative mx-auto mt-20 flex w-full flex-col items-center justify-center gap-10 lg:w-3/4"
-  >
+  <main class="relative mx-auto mt-20 flex w-full flex-col items-center justify-center gap-10 lg:w-3/4">
     <TheProgress />
     <div class="relative w-full rounded-md bg-slate-700/30">
+      <TheToaster v-if="error" :message="error" @close="error = null" />
       <Suspense>
         <template #default>
           <SelectHotel />
@@ -20,14 +19,18 @@
 </template>
 
 <script setup lang="ts">
-import { useStore } from 'vuex'
 import TheNavBar from './components/TheNavBar.vue'
 import TheProgress from './components/TheProgress.vue'
-import type State from './store/state.model'
 import SelectHotel from './components/SelectHotel.vue'
 import TheSelectbox from './components/TheSelectbox.vue'
+import { onErrorCaptured, ref } from 'vue'
+import TheToaster from './components/TheToastr.vue'
 
-const store = useStore<State>()
+const error = ref<Error | null>(null)
+
+onErrorCaptured((err) => {
+  error.value = err
+})
 </script>
 
 <style>
