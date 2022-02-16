2a6f8132632bfed1cc0d06bca9f879e9a448c326
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-16 19:05:13 +0300

    feature. getFeatures from store listen changes

--- src/components/SelectHotel.vue
+++ src/components/SelectHotel.vue
@@ -1,27 +1,38 @@
 <template>
   <section class="mx-5 my-8">
-    <TheSelectbox placeholder="Select a hotel" :options="hotels" @change="selectHotel($event)"></TheSelectbox>
+    <TheSelectbox
+      placeholder="Select a hotel"
+      :options="getHotels"
+      value=""
+      @change="selectHotel($event)"
+    ></TheSelectbox>
   </section>
 </template>
 
 <script setup lang="ts">
 import { useStore } from 'vuex'
 import TheSelectbox from './TheSelectbox.vue'
-import type Hotel from '../models/Hotel.model'
-import type { Ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 
 const store = useStore()
-let hotels: Ref<Hotel[]>
+
+let selectedHotelId = ref<string>('')
+let hotelIdsNames = ref<{ id: string; hotel_name: string }[]>([])
 
 try {
-  hotels = await store.dispatch('getHotels')
+  hotelIdsNames = await store.dispatch('getHotels')
 } catch (error: any) {
-  console.error(error.message)
+  throw new Error(error.message)
 }
 
+const getHotels = computed(() => hotelIdsNames.value.map((hotel) => ({ id: hotel.id, name: hotel.hotel_name })))
+
+watch(selectedHotelId, (id: string) => {
+  store.dispatch('setSelectedHotel', id)
+})
+
 const selectHotel = (e: any) => {
-  const selectedHotel = hotels.value.find((hotel) => hotel.id === e.target.value)
-  store.dispatch('setSelectedHotel', selectedHotel)
+  selectedHotelId.value = e.target.value
 }
 </script>
 
