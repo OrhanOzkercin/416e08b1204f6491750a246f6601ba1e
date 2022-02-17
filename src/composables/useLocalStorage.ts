14da4fb1cbce8e9412908f7ed955138bef167b38
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-17 13:46:24 +0300

    fix: useLocalStorage hook

--- src/composables/useLocalStorage.ts
+++ src/composables/useLocalStorage.ts
@@ -1,29 +1,24 @@
-import { computed, onMounted, ref, watch } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 
-function useLocalStorageState(
-  key: string,
-  defaultValue: any = '',
-  { serialize = JSON.stringify, deserialize = JSON.parse } = {}
-) {
-  const state = ref()
+export function useLocalStorage<T>(key: string, { serialize = JSON.stringify, deserialize = JSON.parse } = {}) {
+  const localStorageValue = ref<T>()
 
   onMounted(() => {
     const valueInLocalStorage = window.localStorage.getItem(key)
     if (valueInLocalStorage) {
-      state.value = deserialize(valueInLocalStorage)
+      localStorageValue.value = deserialize(valueInLocalStorage)
     }
-    state.value = typeof defaultValue === 'function' ? defaultValue() : defaultValue
   })
 
   const prevKeyRef = ref(key)
-  watch([state, prevKeyRef], (_, [newKey, oldKey]): void => {
+  watch(prevKeyRef, (newKey, oldKey): void => {
     if (newKey !== oldKey) {
       window.localStorage.removeItem(oldKey)
     }
-    window.localStorage.setItem(prevKeyRef.value, serialize(state.value))
   })
 
-  return { key: prevKeyRef, state }
+  watch(localStorageValue, (): void => {
+    window.localStorage.setItem(prevKeyRef.value, serialize(localStorageValue.value))
+  })
+  return { localStorageKey: prevKeyRef, localStorageValue }
 }
-
-export { useLocalStorageState }
