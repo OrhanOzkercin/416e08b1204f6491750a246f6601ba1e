774361de5d2dac44bdbe0585a0d136139527337d
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-18 02:48:04 +0300

    refactor: models

---
+++ src/models/Coupon.model.ts
@@ -0,0 +1,6 @@
+export default interface Coupon {
+  code: string
+  discount_ammount: number
+  expiration_at: string
+  id: string
+}
---
+++ src/models/LocalStorage.model.ts
@@ -0,0 +1,11 @@
+export default interface LocalStorage {
+  hotel_id?: string
+  hotel_name?: string
+  adult?: number
+  child?: number
+  start_date?: Date
+  end_date?: Date
+  room_type?: string
+  room_scenic?: string
+  price?: number
+}
