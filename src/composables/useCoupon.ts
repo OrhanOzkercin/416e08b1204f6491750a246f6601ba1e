41dcd906fe8e4143ee40a485b7b79467fa77aa0e
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-18 02:47:42 +0300

    feature: create useCoupons composable

---
+++ src/composables/useCoupon.ts
@@ -0,0 +1,28 @@
+import type Coupon from '@/models/Coupon.model'
+import { dateDifference } from '@/utils'
+import { ref } from 'vue'
+import { useFetch } from './useFetch'
+export async function useCoupon(couponCode: string) {
+  const discoundedAmount = ref<number>()
+  const { data, error, fetchData } = useFetch<Coupon[]>('/coupons', `code=${couponCode}`)
+  await fetchData()
+  if (error.value) throw new Error(error.value.message)
+  if (data.value) {
+    const discount = data.value.find((coupon) => {
+      console.log(1)
+
+      if (coupon.code !== couponCode.toUpperCase()) {
+        return false
+      }
+      return dateDifference(new Date(), new Date(coupon.expiration_at)) > 0
+    })
+    console.log('discount:', discount)
+    if (!discount) {
+      error.value = new Error('Invalid Coupon')
+    } else {
+      discoundedAmount.value = discount.discount_ammount
+    }
+  }
+
+  return { discoundedAmount, error }
+}
