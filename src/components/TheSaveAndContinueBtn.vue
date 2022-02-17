68ef3ba1a8518b9ae3cea3f31bfb1536c74b6df1
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-18 02:51:20 +0300

    feature: add back ability

--- src/components/TheSaveAndContinueBtn.vue
+++ src/components/TheSaveAndContinueBtn.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="w-full rounded-md bg-slate-700/30 px-8 py-7">
-    <button :disabled="disabled" @click="emit('click')" type="button" class="btn my-auto mx-auto block w-full lg:w-1/2">
+  <div class="flex w-full rounded-md bg-slate-700/30 px-8 py-7">
+    <button
+      v-if="isBackActive"
+      @click="emit('back')"
+      type="button"
+      class="btn lg:text-md my-auto mx-auto mr-5 block w-full px-10 py-4 text-xs lg:w-1/2"
+    >
+      <slot name="back"> Back</slot>
+    </button>
+    <button
+      :disabled="disabled"
+      @click="emit('save')"
+      type="button"
+      class="btn lg:text-md my-auto mx-auto block w-full px-10 py-4 text-xs lg:w-1/2"
+    >
       <slot> Save and Continue </slot>
     </button>
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps<{ disabled: boolean }>()
-const emit = defineEmits(['click'])
+withDefaults(defineProps<{ disabled: boolean; isBackActive?: boolean }>(), {
+  disabled: false,
+  isBackActive: false
+})
+const emit = defineEmits(['save', 'back'])
 </script>
