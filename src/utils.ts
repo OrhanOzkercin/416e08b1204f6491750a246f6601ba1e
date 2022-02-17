7904087d9e3d777e8f550369f348cb6faaa45a86
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-18 02:47:20 +0300

    feature: create utils methods

---
+++ src/utils.ts
@@ -0,0 +1,16 @@
+export const dateDifference = (date1: any, date2: any) => {
+  const d1 = new Date(date1)
+  const d2 = new Date(date2)
+  const diffTime = Math.abs(d2 - d1)
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+  return diffDays
+}
+
+export const moneyFormatter = (price: number) => {
+  const formatedPrice = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0
+  }).format(price)
+  return formatedPrice
+}
