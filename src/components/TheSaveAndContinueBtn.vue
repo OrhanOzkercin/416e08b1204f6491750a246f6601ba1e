c75455da80c1871559ddcf8dbf0136389373f446
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-17 13:48:22 +0300

    feature: add saveAndcontinue button

---
+++ src/components/TheSaveAndContinueBtn.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full rounded-md bg-slate-700/30 px-8 py-7">
+    <button :disabled="disabled" @click="emit('click')" type="button" class="btn my-auto mx-auto block w-full lg:w-1/2">
+      <slot> Save and Continue </slot>
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ disabled: boolean }>()
+const emit = defineEmits(['click'])
+</script>
