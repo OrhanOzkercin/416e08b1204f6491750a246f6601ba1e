09846baf062693bdba868caf258dbd53dfd1a463
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-17 13:45:46 +0300

    feature: add datepicker package

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
+import VueDatepickerUi from 'vue-datepicker-ui'
+import 'vue-datepicker-ui/lib/vuedatepickerui.css'
+
 import { createApp } from 'vue'
+import '@/assets/datepicker.css'
 import App from './App.vue'
 
 import store from './store'
 const app = createApp(App)
+// eslint-disable-next-line vue/multi-word-component-names
+app.component('Datepicker', VueDatepickerUi)
+
 app.use(store)
 app.mount('#app')
