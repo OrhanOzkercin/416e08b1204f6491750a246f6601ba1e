253609cdc62f0eb6a65d2e3bbf04a49f35d6273b
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-18 02:49:42 +0300

    fix: texts in navbar

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -1,10 +1,8 @@
 <template>
   <nav class="flex justify-between border-b border-slate-50/[0.1] pb-2">
-    <h1>Otel Rezervasyon</h1>
-    <button
-      class="rounded-md border-2 border-primary/80 px-4 text-sm hover:border-primary hover:shadow-md"
-    >
-      Rezervasyon Yap
+    <h1>Hotel Reservation</h1>
+    <button class="rounded-md border-2 border-primary/80 px-4 text-sm hover:border-primary hover:shadow-md">
+      Make a reservation
     </button>
   </nav>
 </template>
