8de20a1f2c6b4c7f0b6b164dc01e3077eb80c1cf
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-18 02:49:02 +0300

    fix: removed used pieces from useLocalStorage composable

--- src/composables/useLocalStorage.ts
+++ src/composables/useLocalStorage.ts
@@ -1,24 +1,30 @@
 import { onMounted, ref, watch } from 'vue'
 
-export function useLocalStorage<T>(key: string, { serialize = JSON.stringify, deserialize = JSON.parse } = {}) {
+export function useLocalStorage<T>(
+  key: string,
+  defaultValue: any = '',
+  { serialize = JSON.stringify, deserialize = JSON.parse } = {}
+) {
   const localStorageValue = ref<T>()
-
   onMounted(() => {
     const valueInLocalStorage = window.localStorage.getItem(key)
     if (valueInLocalStorage) {
       localStorageValue.value = deserialize(valueInLocalStorage)
-    }
-  })
-
-  const prevKeyRef = ref(key)
-  watch(prevKeyRef, (newKey, oldKey): void => {
-    if (newKey !== oldKey) {
-      window.localStorage.removeItem(oldKey)
+    } else {
+      localStorageValue.value = defaultValue
+      window.localStorage.setItem(key, serialize(localStorageValue.value))
     }
   })
 
   watch(localStorageValue, (): void => {
-    window.localStorage.setItem(prevKeyRef.value, serialize(localStorageValue.value))
+    window.localStorage.setItem(key, serialize(localStorageValue.value))
   })
-  return { localStorageKey: prevKeyRef, localStorageValue }
+
+  const updateLocalStorageValue = () => {
+    const valueInLocalStorage = window.localStorage.getItem(key)
+    if (valueInLocalStorage) {
+      localStorageValue.value = deserialize(valueInLocalStorage)
+    }
+  }
+  return { updateLocalStorageValue, localStorageValue }
 }
