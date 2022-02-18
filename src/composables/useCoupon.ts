40ca09d17d93d44126120400229b78f0510139cc
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-18 20:57:27 +0300

    fix: indentation, site name and favicon

--- src/composables/useCoupon.ts
+++ src/composables/useCoupon.ts
@@ -4,9 +4,11 @@ import { ref } from 'vue'
 import { useFetch } from './useFetch'
 export async function useCoupon(couponCode: string) {
   const discoundedAmount = ref<number>()
+
   const { data, error, fetchData } = useFetch<Coupon[]>('/coupons', `code=${couponCode}`)
   await fetchData()
   if (error.value) throw new Error(error.value.message)
+
   if (data.value) {
     const discount = data.value.find((coupon) => {
       if (coupon.code !== couponCode.toUpperCase()) {
@@ -14,6 +16,7 @@ export async function useCoupon(couponCode: string) {
       }
       return dateDifference(new Date(), new Date(coupon.expiration_at)) > 0
     })
+
     if (!discount) {
       error.value = new Error('Invalid Coupon')
     } else {
