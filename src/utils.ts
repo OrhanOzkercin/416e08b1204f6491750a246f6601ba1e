434073e410b8dc99e2fbff91f0eeabb17cad4e98
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-18 19:06:43 +0300

    feature: add  formatDate utils fix dateDiffrence

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,12 @@
+export const formatDateYYYYMMDD = (date: Date) => {
+  if (!date) return
+  const d = new Date(date)
+  return d.toISOString().split('T')[0]
+}
 export const dateDifference = (date1: any, date2: any) => {
   const d1 = new Date(date1)
   const d2 = new Date(date2)
-  const diffTime = Math.abs(d2 - d1)
+  const diffTime = d2 - d1
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
   return diffDays
 }
