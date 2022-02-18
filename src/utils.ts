b40378c2cae838ab482ea68c177648c191e9519c
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-18 19:17:12 +0300

    fix: type convertion

--- src/utils.ts
+++ src/utils.ts
@@ -6,7 +6,7 @@ export const formatDateYYYYMMDD = (date: Date) => {
 export const dateDifference = (date1: any, date2: any) => {
   const d1 = new Date(date1)
   const d2 = new Date(date2)
-  const diffTime = d2 - d1
+  const diffTime = +d2 - +d1
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
   return diffDays
 }
