bb8311123df54a9eacd0c98ac2aa25300e8b86d1
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-18 19:59:54 +0300

    fix: Turkish words

--- src/components/TheProgress.vue
+++ src/components/TheProgress.vue
@@ -1,18 +1,8 @@
 <template>
-  <section
-    class="flex w-full justify-around gap-8 rounded-md bg-slate-700/30 px-1 py-7 md:gap-20 md:py-10 md:px-10"
-  >
-    <div
-      class="text-center"
-      v-for="(option, index) in options"
-      :key="option.label"
-    >
+  <section class="flex w-full justify-around gap-8 rounded-md bg-slate-700/30 px-1 py-7 md:gap-20 md:py-10 md:px-10">
+    <div class="text-center" v-for="(option, index) in options" :key="option.label">
       <TheProgressCard :class="{ 'border-primary': activeCard === index }">
-        <component
-          :is="option.iconComponent"
-          :size="32"
-          :fillColor="activeCard === index ? '#eab308' : '#fff'"
-        />
+        <component :is="option.iconComponent" :size="32" :fillColor="activeCard === index ? '#eab308' : '#fff'" />
       </TheProgressCard>
       <span
         class="mt-3 inline-block max-w-[11rem] break-words text-sm font-bold"
@@ -33,9 +23,9 @@ import PaymentIcon from '@/assets/icons/PaymentIcon.vue'
 import { computed } from 'vue'
 
 const options = [
-  { label: 'Otel ve Tarih', iconComponent: CalendarIcon },
-  { label: 'Oda ve Manzara', iconComponent: SleepIcon },
-  { label: 'Önizleme ve Ödeme', iconComponent: PaymentIcon }
+  { label: 'Hotel and time', iconComponent: CalendarIcon },
+  { label: 'Room and View', iconComponent: SleepIcon },
+  { label: 'Preview and Payment', iconComponent: PaymentIcon }
 ]
 
 const store = useStore<State>()
