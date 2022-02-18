5aeeb18f9bde9645165319391d1a33d919465b9d
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-18 19:10:00 +0300

    feature: add creditcard and localstorage models

---
+++ src/models/CreditCard.model.ts
@@ -0,0 +1,7 @@
+export default interface CreditCard {
+  cardName: string
+  cardNumber: string
+  cardMonth: string
+  cardYear: string
+  cardCvv: string
+}
--- src/models/LocalStorage.model.ts
+++ src/models/LocalStorage.model.ts
@@ -3,9 +3,15 @@ export default interface LocalStorage {
   hotel_name?: string
   adult?: number
   child?: number
-  start_date?: Date
-  end_date?: Date
+  start_date?: Date | string
+  end_date?: Date | string
   room_type?: string
   room_scenic?: string
   price?: number
+  card_name?: string
+  card_number?: string
+  card_date_month?: string
+  card_date_year?: string
+  card_cvv?: string
+  coupon_code?: string
 }
